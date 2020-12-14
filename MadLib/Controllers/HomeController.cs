859f69076d6c078b30edf5eb5e068257bfed11c7
Tawnee Harris <tawneeh@icloud.com>
2020-12-14 15:16:36 -0800

    add return View method for Gettysburg
    Co-authored-by: Ben McFarland <benrmcfarland@gmail.com>

--- MadLib/Controllers/HomeController.cs
+++ MadLib/Controllers/HomeController.cs
@@ -12,22 +12,35 @@ namespace MadLib.Controllers
     [Route("/xmas-form")]
     public ActionResult XmasForm() { return View(); }
 
+
     [Route("/xmas-final")] // must match form action in .cshtml
     public ActionResult XmasFinal(string animal, string holiday, string name, string pastVerb, string clothing, string season, string noise, string objects, string number, string noun, string fast)
     {
-      WordVariable myWordVariable = new WordVariable();
-      myWordVariable.Animal = animal;
-      myWordVariable.Holiday = holiday;
-      myWordVariable.Name = name;
-      myWordVariable.PastVerb = pastVerb;
-      myWordVariable.Clothing = clothing;
-      myWordVariable.Season = season;
-      myWordVariable.Noise = noise;
-      myWordVariable.Objects = objects;
-      myWordVariable.Number = number;
-      myWordVariable.Noun = noun;
-      myWordVariable.Fast = fast;
-      return View(myWordVariable);
+      WordVariable myXmasWordVariable = new WordVariable();
+      myXmasWordVariable.Animal = animal;
+      myXmasWordVariable.Holiday = holiday;
+      myXmasWordVariable.Name = name;
+      myXmasWordVariable.PastVerb = pastVerb;
+      myXmasWordVariable.Clothing = clothing;
+      myXmasWordVariable.Season = season;
+      myXmasWordVariable.Noise = noise;
+      myXmasWordVariable.Objects = objects;
+      myXmasWordVariable.Number = number;
+      myXmasWordVariable.Noun = noun;
+      myXmasWordVariable.Fast = fast;
+      return View(myXmasWordVariable);
     }
+
+    [Route("/gett-form")]
+    public ActionResult GettForm() { return View(); }
+
+    [Route("/gett-final")]
+    public ActionResult GettFinal(string number) 
+    {
+    WordVariable myGettWordVariable = new WordVariable();
+    myGettWordVariable.Number = number;
+    return View(myGettWordVariable);
+    }
+
   }
 }
