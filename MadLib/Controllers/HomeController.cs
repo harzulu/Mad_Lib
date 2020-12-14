ed6c6b1e251cbb292a705cb875b8d51cd7156963
Tawnee Harris <tawneeh@icloud.com>
2020-12-14 14:12:47 -0800

    add word variables
    Co-authored-by: Ben McFarland <benrmcfarland@gmail.com>

--- MadLib/Controllers/HomeController.cs
+++ MadLib/Controllers/HomeController.cs
@@ -9,11 +9,21 @@ namespace MadLib.Controllers
     [Route("/")]
     public ActionResult Form() { return View(); }
 
-    [Route("/finalMadLib")] // must match form action
-    public ActionResult Final(string animal)
+    [Route("/finalMadLib")] // must match form action in .cshtml
+    public ActionResult Final(string animal, string holiday, string name, string pastVerb, string clothing, string season, string noise, string objects, string number, string noun, string fast)
     {
       WordVariable myWordVariable = new WordVariable();
       myWordVariable.Animal = animal;
+      myWordVariable.Holiday = holiday;
+      myWordVariable.Name = name;
+      myWordVariable.PastVerb = pastVerb;
+      myWordVariable.Clothing = clothing;
+      myWordVariable.Season = season;
+      myWordVariable.Noise = noise;
+      myWordVariable.Objects = objects;
+      myWordVariable.Number = number;
+      myWordVariable.Noun = noun;
+      myWordVariable.Fast = fast;
       return View(myWordVariable);
     }
   }
