646d7b96e4d88d576ae0fafc480e1a83f70b4f2a
Tawnee Harris <tawneeh@icloud.com>
2020-12-14 14:13:52 -0800

    add getters and setters
    Co-authored-by: Ben McFarland <benrmcfarland@gmail.com>

--- MadLib/Models/WordVariable.cs
+++ MadLib/Models/WordVariable.cs
@@ -4,6 +4,25 @@ namespace MadLib.Models
   {
     public string Animal { get; set; }
 
+    public string Holiday { get; set; }
+
+    public string Name { get; set; }
+
+    public string PastVerb { get; set; }
+
+    public string Clothing { get; set; }
+
+    public string Season { get; set; }
+
+    public string Noise { get; set; }
+
+    public string Objects { get; set; }
+
+    public string Number { get; set; }
+
+    public string Noun { get; set; }
+
+    public string Fast { get; set; }
   }
 
 }
